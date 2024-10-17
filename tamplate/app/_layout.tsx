6a2d8c4d85a983a48e626052f002aa757d61fd91
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2024-10-17 18:00:02 +0300

    started the second lesson

--- tamplate/app/_layout.tsx
+++ tamplate/app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index-2" options={{ headerTitle: "Home" }} />
+      <Stack.Screen name="about" />
     </Stack>
   );
 }
--- tamplate/app/index-2.tsx
+++ tamplate/app/index-2.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
@@ -13,5 +13,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
