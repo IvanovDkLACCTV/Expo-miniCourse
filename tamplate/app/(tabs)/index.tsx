87386ad25b5c48b09cd70b13dcf4b326e38b20f9
Dik131 <d.i.k.93@mail.ru>
2024-10-21 20:27:01 +0300

    added button
    added styles for button

--- tamplate/app/(tabs)/index.tsx
+++ tamplate/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import { View, StyleSheet } from "react-native";
 import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
-const PlaceholderImage = require("../../assets/images/background-image.png")
+const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -21,6 +26,9 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-  }
-
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
 });
