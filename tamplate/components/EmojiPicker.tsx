13b4427a3b1df2866892baffd867dc33b100ea1d
Dik131 <d.i.k.93@mail.ru>
2024-10-23 20:53:50 +0300

    children was in the wrong view

--- tamplate/components/EmojiPicker.tsx
+++ tamplate/components/EmojiPicker.tsx
@@ -1,51 +1,62 @@
-import { Modal, View, Text, Pressable, StyleSheet } from "react-native"
-import MaterialIcons from "@expo/vector-icons/MaterialIcons"
+import { Modal, View, Text, Pressable, StyleSheet } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 type Props = {
-  isVisible: boolean
-  children: React.ReactNode
-  onClose: () => void
-}
+  isVisible: boolean;
+  children: React.ReactNode;
+  onClose: () => void;
+};
 
 export default function EmojiPicker({ isVisible, children, onClose }: Props) {
   return (
-    <Modal animationType="slide" visible={isVisible} transparent={true}>
+    <Modal
+      animationType="slide"
+      visible={isVisible}
+      transparent={true}>
       <View style={style.modalContent}>
         <View style={style.titleContainer}>
           <Text style={style.title}>Choose your emoji</Text>
           <Pressable onPress={onClose}>
-            <MaterialIcons name="close" size={22} color="#fff" />
+            <MaterialIcons
+              name="close"
+              size={22}
+              color="#fff"
+            />
           </Pressable>
         </View>
-        {children}
+        <View style={style.pickerContainer}>{children}</View>
       </View>
-      <View style={style.modalContent}>{children}</View>
     </Modal>
-  )
+  );
 }
 
 const style = StyleSheet.create({
   title: {
-    color: "#fff",
+    color: '#fff',
     fontSize: 16,
   },
   titleContainer: {
-    alignItems: "center",
-    height: "16%",
-    justifyContent: "space-between",
-    backgroundColor: "#464C55",
-    flexDirection: "row",
+    alignItems: 'center',
+    height: '16%',
+    justifyContent: 'space-between',
+    backgroundColor: '#464C55',
+    flexDirection: 'row',
     paddingHorizontal: 20,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
   },
   modalContent: {
-    backgroundColor: "#25292e",
-    height: "25%",
-    width: "100%",
-    position: "absolute",
+    backgroundColor: '#25292e',
+    height: '25%',
+    width: '100%',
+    position: 'absolute',
     borderTopLeftRadius: 18,
     borderTopRightRadius: 18,
     bottom: 0,
   },
-})
+  pickerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
