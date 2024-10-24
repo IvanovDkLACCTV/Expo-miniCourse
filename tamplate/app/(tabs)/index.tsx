321dae708b0cad905f21178e49fc354b01b62d5f
Dik131 <d.i.k.93@mail.ru>
2024-10-24 20:36:07 +0300

    added EmojiSticker
    but have an error in EmojiList with horizontal

--- tamplate/app/(tabs)/index.tsx
+++ tamplate/app/(tabs)/index.tsx
@@ -1,61 +1,73 @@
 //outer components
-import { View, StyleSheet } from "react-native"
-import { useState } from "react"
-import * as ImagePicker from "expo-image-picker"
+import { View, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 //inner components
-import ImageViewer from "@/components/ImageViewer"
-import Button from "@/components/Button"
-import IconButton from "@/components/IconButton"
-import CircleButton from "@/components/CircleButton"
-import EmojiPicker from "@/components/EmojiPicker"
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
+import EmojiSticker from '@/components/EmojiSticker';
 
-const PlaceholderImage = require("../../assets/images/background-image.png")
+const PlaceholderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
   //states
-  const [selectImage, setSelectImage] = useState<string | undefined>(undefined)
-  const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false)
-
+  const [selectImage, setSelectImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<string | undefined>(undefined);
   //functions
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
       quality: 0.1,
-    })
+    });
     if (!result.canceled) {
-      setSelectImage(result.assets[0].uri)
-      setShowAppOptions(true)
-      console.log(result)
+      setSelectImage(result.assets[0].uri);
+      setShowAppOptions(true);
+      console.log(result);
     } else {
-      alert("You did not select any image.")
+      alert('You did not select any image.');
     }
-  }
+  };
 
   const onReset = () => {
-    setShowAppOptions(false)
-  }
+    setShowAppOptions(false);
+  };
 
   const onModalClose = () => {
-    setIsModalVisible(false)
-  }
+    setIsModalVisible(false);
+  };
 
   const onAddSticker = () => {
-    setIsModalVisible(true)
-  }
+    setIsModalVisible(true);
+  };
 
-  const onSaveImageAsync = () => {}
+  const onSaveImageAsync = () => {};
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectImage || PlaceholderImage} />
+        {pickedEmoji && (
+          <EmojiSticker
+            imageSize={40}
+            stickerSource={pickedEmoji}
+          />
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
-            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <IconButton
+              icon="refresh"
+              label="Reset"
+              onPress={onReset}
+            />
             <CircleButton onPress={onAddSticker} />
             <IconButton
               icon="save-alt"
@@ -77,33 +89,38 @@ export default function Index() {
           />
         </View>
       )}
-      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <></>
+      <EmojiPicker
+        isVisible={isModalVisible}
+        onClose={onModalClose}>
+        <EmojiList
+          onSelect={setPickedEmoji}
+          onCloseModal={onModalClose}
+        />
       </EmojiPicker>
     </View>
-  )
+  );
 }
 
 //styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    backgroundColor: "#25292e",
+    alignItems: 'center',
+    backgroundColor: '#25292e',
   },
   imageContainer: {
     flex: 1,
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: "center",
+    alignItems: 'center',
   },
   optionsContainer: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 80,
   },
   optionsRow: {
-    alignItems: "center",
-    flexDirection: "row",
+    alignItems: 'center',
+    flexDirection: 'row',
   },
-})
+});
--- tamplate/components/EmojiList.tsx
+++ tamplate/components/EmojiList.tsx
@@ -1,54 +1,57 @@
 //outer components
-import { useState } from "react";
-import { FlatList, StyleSheet, Platform, Pressable } from "react-native";
-import { Image } from "expo-image";
+import { useState } from 'react';
+import { FlatList, StyleSheet, Platform, Pressable } from 'react-native';
+import { Image } from 'expo-image';
 
 type Props = {
-    onSelect: (image: string) => void;
-    onCloseModal: () => void;
+  onSelect: (image: string) => void;
+  onCloseModal: () => void;
 };
 
 export default function EmojiList({ onSelect, onCloseModal }: Props) {
-    const [emoji] = useState([
-        require("../assets/images/emoji-2.png"),
-        require("../assets/images/emoji-1.png"),
-        require("../assets/images/emoji-3.png"),
-        require("../assets/images/emoji-4.png"),
-        require("../assets/images/emoji-5.png"),
-        require("../assets/images/emoji-6.png"),
-    ])
+  const [emoji] = useState([
+    require('../assets/images/emoji1.png'),
+    require('../assets/images/emoji2.png'),
+    require('../assets/images/emoji3.png'),
+    require('../assets/images/emoji4.png'),
+    require('../assets/images/emoji5.png'),
+    require('../assets/images/emoji6.png'),
+  ]);
 
-    return (
-        <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={Platform.OS === "web" }
-            data={emoji}
-            contentContainerStyle={styles.listContainer}
-            renderItem={({ item, index }) => (
-                <Pressable
-                    onPress={() => {
-                        onSelect(item)
-                        onCloseModal()
-                    }}
-                >
-                    <Image source={item} key={index} style={styles.image} />
-                </Pressable>
-            )}
-            numColumns={3}
-        />
-    )
+  return (
+    <FlatList
+      //horizontal
+      showsHorizontalScrollIndicator={Platform.OS === 'web'}
+      data={emoji}
+      contentContainerStyle={styles.listContainer}
+      renderItem={({ item, index }) => (
+        <Pressable
+          onPress={() => {
+            onSelect(item);
+            onCloseModal();
+          }}>
+          <Image
+            source={item}
+            key={index}
+            style={styles.image}
+          />
+        </Pressable>
+      )}
+      numColumns={3}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
-    listContainer: {
-        borderTopRightRadius: 10,
-        borderTopLeftRadius: 10,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    image: {
-        width: 100,
-        height: 100,
-        margin: 10,
-    },
-})
+  listContainer: {
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+});
